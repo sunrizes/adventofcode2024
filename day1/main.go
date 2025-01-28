2f5da3ca5bbfd221d16faee17ec4d8d688629150
sunrizes <sunrizes99@gmail.com>
2025-01-28 20:49:51 +0100

    File reading

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,37 @@
+// Day 1 - Hystorian Hysteria
+
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	temp := readFile("input.txt")
+
+	for _, v := range temp {
+		fmt.Println(v)
+	}
+}
+
+func readFile(fileName string) []string {
+	input, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer input.Close()
+
+	scanner := bufio.NewScanner(input)
+
+	var temp = []string{}
+
+	for scanner.Scan() {
+		temp = append(temp, scanner.Text())
+	}
+
+	return temp
 }
