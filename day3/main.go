e0ac821dcb93be57043090d90bb6c8dd44b4c521
sunrizes <sunrizes99@gmail.com>
2025-02-06 23:40:52 +0100

    Part one done

--- day3/main.go
+++ day3/main.go
@@ -6,16 +6,44 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func main() {
 	input := readFile("input.txt")
-	fmt.Println(getMul(input))
+	muls := getMul(input)
+
+	var total int = 0
+
+	for i := 0; i < len(muls); i++ {
+		total += getProduct(getMultipliers(muls[i]))
+	}
+
+	fmt.Println(total)
 }
 
 func getMul(s string) []string {
 	re := regexp.MustCompile("(?i)mul\\([A-Za-z0-9]+,[A-Za-z0-9]+\\)")
 	return re.FindAllString(s, -1)
+}
+
+func getMultipliers(s string) []string {
+	re := regexp.MustCompile("[0-9]+")
+	return re.FindAllString(s, -1)
+}
+
+func getProduct(sa []string) int {
+	num1, err := strconv.Atoi(sa[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	num2, err := strconv.Atoi(sa[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return num1 * num2
 }
 
 func readFile(fileName string) string {
