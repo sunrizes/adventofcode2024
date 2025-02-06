9e99dbe781cbdac39e12b78615e7dfaf4b06a0e9
sunrizes <sunrizes99@gmail.com>
2025-02-06 23:22:39 +0100

    txt input added, regex function

--- day3/main.go
+++ day3/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
 
+func main() {
+	input := readFile("input.txt")
+	fmt.Println(input)
+}
+
+func getMul(s string) bool {
+	re := regexp.MustCompile("(?i)mul\\([A-Za-z0-9]+,[A-Za-z0-9]+\\)")
+	return re.MatchString(s)
+}
+
+func readFile(fileName string) []string {
+	input, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer input.Close()
+
+	scanner := bufio.NewScanner(input)
+
+	var output = []string{}
+
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+
+	return output
 }
